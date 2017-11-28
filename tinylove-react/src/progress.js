8b938284428f92b51fa54ed5debd2a5852305800
JAQUELINE KIMMI KIRINO <34568531810@jpmorgan.sp>
2017-11-28 21:40:36 -0200

    Add progress variables

---
+++ tinylove-react/src/progress.js
@@ -0,0 +1,20 @@
+// Hero variables
+var logo = 'progress';
+var hero = {
+  description: 'A new way for services',
+  image: '../img/cellphone.jpg'
+};
+
+// Services section
+var services = {
+  title: 'Track lalal',
+  description: 'Lorem ipsum',
+  image: '../img/imagem.png'
+}
+
+// Form byob
+var email = 'deisearruda@gmail.com';
+
+// byob image component
+var imageByob = '../img/imagem';
+
