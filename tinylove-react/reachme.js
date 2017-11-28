acc05945bbcba23b72a41d560c82d60968051b65
SHIRLEY ANA OVANDO PAZ <37024731818@jpmorgan.sp>
2017-11-28 21:36:40 -0200

    reacme identificacao js

---
+++ tinylove-react/reachme.js
@@ -0,0 +1,34 @@
+var hero = {
+    title: "Reach.me features",
+    description: "scale automate..",
+    button: "Learn more",
+    image: "./components/Hero/hero_IMG_W768xH373_.jpg"
+    /*background: "./img/hero_BG_W_1440xH568.jpg" */ 
+}
+var features = {
+    title: "Social Publishing",
+    description: "Draft...",
+    button: "Get started",
+    image: "./components/Features/DSK_imac_large.png"
+}
+
+
+ /*
+var features = {
+    title: "Engagement",
+    description: "See a full...",
+    button: "Get started",
+    image: "./img/dgzerytw5e543_.jpg"
+}
+var features = {
+    title: "Brand Monitoring",
+    description: "Set up...",
+    button: "Get started",
+    image: "./img/dgzerytw5e543_.jpg"
+}
+var features = {
+    title: "Analytics",
+    description: "Get insight...",
+    button: "Get started",
+    image: "./img/dgzerytw5e543_.jpg"
+}
